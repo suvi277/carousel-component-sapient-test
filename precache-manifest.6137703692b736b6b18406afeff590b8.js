ce565597f8b8a7da5676d67ba09a4063563c5d4f
Suvarna Mondal <suvarnamondal27@gmail.com>
2020-03-30 07:03:57 +0100

    Updates

---
+++ precache-manifest.6137703692b736b6b18406afeff590b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa6646a3e4eee673d2c8c580fef599b",
+    "url": "/carousel-component-sapient-test/index.html"
+  },
+  {
+    "revision": "63e17a314484b1718b6b",
+    "url": "/carousel-component-sapient-test/static/css/main.833a6889.chunk.css"
+  },
+  {
+    "revision": "c7bef7bff85341a5a350",
+    "url": "/carousel-component-sapient-test/static/js/2.6d61e812.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/carousel-component-sapient-test/static/js/2.6d61e812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e17a314484b1718b6b",
+    "url": "/carousel-component-sapient-test/static/js/main.45133f04.chunk.js"
+  },
+  {
+    "revision": "884842ca812755e8020d",
+    "url": "/carousel-component-sapient-test/static/js/runtime-main.cd4c8de9.js"
+  },
+  {
+    "revision": "344562805779fba5425c2e6510f8f41c",
+    "url": "/carousel-component-sapient-test/static/media/arrow.34456280.svg"
+  }
+]);
