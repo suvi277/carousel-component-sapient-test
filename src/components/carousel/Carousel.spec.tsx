3317b32e7bc751d0d093af5a170aac3d7302bd6d
Suvarna Mondal <suvarnamondal27@gmail.com>
2020-03-30 06:09:36 +0100

    updated test cases

--- src/components/carousel/Carousel.spec.tsx
+++ src/components/carousel/Carousel.spec.tsx
@@ -1,13 +1,66 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
-import Carousel from './Carousel';
+import CarouselComponent from './Carousel';
+import {render, fireEvent, getByTestId} from '@testing-library/react'
 
-describe('<Carousel /> with default props', () => {
-	const defaultProps = {
-		photos: []
-	};
-	const wrapper = shallow(<Carousel {...defaultProps} />);
-	it('renders as expected', () => {
-		expect(wrapper).toMatchSnapshot();
+const mockItems = [
+	{
+		previewURL: 'sample url 1',
+		user: 'sample user name 1',
+		likes: 12
+	},
+	{
+		previewURL: 'sample url 2',
+		user: 'sample user name 2',
+		likes: 17
+	},
+	{
+		previewURL: 'sample url 3',
+		user: 'sample user name 3',
+		likes: 40
+	},
+	{
+		previewURL: 'sample url 4',
+		user: 'sample user name 4',
+		likes: 40
+	},
+	{
+		previewURL: 'sample url 5',
+		user: 'sample user name 5',
+		likes: 30
+	},
+	{
+		previewURL: 'sample url 6',
+		user: 'sample user name 6',
+		likes: 62
+	},
+	{
+		previewURL: 'sample url 7',
+		user: 'sample user name 7',
+		likes: 37
+	}
+];
+  
+describe('<CarouselComponent /> ', () => {
+	it("renders as expected", () => {
+		const { getByTestId } = render(<CarouselComponent photos={mockItems} />);
+		const items = getByTestId("carousel-items");
+		expect(items.children.length).toBe(5);
+	});
+
+	it("should active the 3rd element from the photos list", () => {
+		const { getByTestId } = render(<CarouselComponent photos={mockItems} />);
+		const items = getByTestId("carousel-items");
+		expect(items.children[2].textContent).toBe(mockItems[2].user);
+	});
+
+	it("previos and next buttons should work", () => {
+		const { getByTestId } = render(<CarouselComponent photos={mockItems} />);
+		const items = getByTestId("carousel-items");
+		const previous = getByTestId("prev-btn");
+		const next = getByTestId("next-btn");
+		fireEvent.click(next);
+		expect(items.children[2].textContent).toBe(mockItems[3].user)
+		fireEvent.click(previous);
+		expect(items.children[2].textContent).toBe(mockItems[2].user);
 	});
 });
--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -14,7 +14,6 @@ const CarouselComponent: React.FC<Props> = ({ photos = [] }) => {
 	useEffect(() => {
 		setVisibleItemOnScreen();
 		window.addEventListener('resize', setVisibleItemOnScreen);
-
 		return () => {
 			window.removeEventListener('resize', setVisibleItemOnScreen);
 		}
@@ -30,13 +29,13 @@ const CarouselComponent: React.FC<Props> = ({ photos = [] }) => {
 		
 		setVisibleItems(item);
 	}
-  	const prevSlide = () => {
+  	const onPrevClick = () => {
 		const resetToVeryBack = current === 0;
 		const index = resetToVeryBack ? photos.length - 1 : current - 1;
 		setCurrent(index);
 	};
 
-	const nextSlide = () => {
+	const onNextClick = () => {
 		const resetIndex = current === photos.length - 1;
 
 		const index = resetIndex ? 0 : current + 1;
@@ -48,14 +47,14 @@ const CarouselComponent: React.FC<Props> = ({ photos = [] }) => {
   	const carouselItems = slicePhotos.length < visibleItems
     		? [...slicePhotos, ...photos.slice(0, visibleItems - slicePhotos.length) ]
 			: slicePhotos;
-
+	
 	return (
 		<div className="carousel-wrapper">
-			<ul className="carousel-items">
-				{carouselItems.map((photo, index) =><PhotoCard key={index} {...photo}/>)}
+			<ul className="carousel-items" data-testid="carousel-items">
+				{carouselItems.map((photo, index) => <PhotoCard key={index} {...photo}/>)}
 			</ul>
-			<button type="button" onClick={prevSlide} aria-label="previous" className="carousel-button prev">prev</button>
-			<button type="button" onClick={nextSlide} aria-label="next" className="carousel-button next">next</button>
+			<button data-testid="prev-btn" type="button" onClick={onPrevClick} aria-label="previous" className="carousel-button prev">prev</button>
+			<button data-testid="next-btn" type="button" onClick={onNextClick} aria-label="next" className="carousel-button next">next</button>
 		</div>
 	);
 };
